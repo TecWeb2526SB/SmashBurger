7dbdc234ea3f24b747050b7392a851fc0d531839
Dorde <dorde.rossi@gmail.com>
2026-03-20 15:21:30 +0100

    fix breadcrumb

--- src/views/template/breadcrumb.php
+++ src/views/template/breadcrumb.php
@@ -9,22 +9,21 @@
 if (empty($breadcrumb)) return;
 ?>
 <nav class="breadcrumb" aria-label="Percorso di navigazione">
-    <div class="contenitore">
-        <ol>
-            <?php foreach ($breadcrumb as $i => $voce):
-                [$etichetta, $href] = $voce;
-                $isUltima = ($i === array_key_last($breadcrumb));
-            ?>
-                <li <?php echo $isUltima ? 'aria-current="page"' : ''; ?>>
-                    <?php if (!$isUltima && $href): ?>
-                        <a href="<?php echo htmlspecialchars($href, ENT_QUOTES, 'UTF-8'); ?>">
-                            <?php echo htmlspecialchars($etichetta, ENT_QUOTES, 'UTF-8'); ?>
-                        </a>
-                    <?php else: ?>
+    <ol>
+        <?php foreach ($breadcrumb as $i => $voce):
+            [$etichetta, $href] = $voce;
+            $isUltima = ($i === array_key_last($breadcrumb));
+        ?>
+            <li <?php echo $isUltima ? 'aria-current="page"' : ''; ?>>
+                <?php if (!$isUltima && $href): ?>
+                    <a href="<?php echo htmlspecialchars($href, ENT_QUOTES, 'UTF-8'); ?>">
                         <?php echo htmlspecialchars($etichetta, ENT_QUOTES, 'UTF-8'); ?>
-                    <?php endif; ?>
-                </li>
-            <?php endforeach; ?>
-        </ol>
-    </div>
+                    </a>
+                <?php else: ?>
+                    <?php echo htmlspecialchars($etichetta, ENT_QUOTES, 'UTF-8'); ?>
+                <?php endif; ?>
+            </li>
+        <?php endforeach; ?>
+    </ol>
 </nav>
+
